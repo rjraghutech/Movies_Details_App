1de8dd2f114e34678c8af6722fbeb96b936d7d16
RJ_Raghu_tech <91701745+rjraghutech@users.noreply.github.com>
2022-08-28 19:48:04 +0530

    committ 3

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // https://api.tvmaze.com/
+
+let search = document.querySelector("#search");
+
+search.addEventListener("keyup",(event => {
+    fetch('https://api.tvmaze.com/search/shows?q=girls')
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}));
