e8106f56926f1e9d24b6c3bcf28f07471ae3dd5f
RJ_Raghu_tech <91701745+rjraghutech@users.noreply.github.com>
2022-08-28 21:59:56 +0530

    commit 9

--- app.js
+++ app.js
@@ -9,23 +9,27 @@ search.addEventListener("keyup",(event => {
     .then((response) => response.json())
     .then((data) => {
         let final = data.map(resfun => resfun.show.name);
-        //console.log(final);
+        console.log(data);
+       // console.log(resfun.show.rating.average);
 
-        
         add_movie.innerHTML = "";
         final.forEach(finalname => {
             let add_div = document.createElement("div");
             add_div.classList.add("movies");
             let movies = document.querySelector(".movies");
+            let rate = data.map(resfun => resfun.show.rating.average);
+            let image = data.map(resfunn => resfunn.show.image.medium);
+            let aboutm = data.map(resfunn => resfunn.show.summary);
+            //console.log(image)
             add_div.innerHTML = `
-            <img src="https://images.indianexpress.com/2022/04/kgf-2-1200.jpeg" alt="Image" height="50%" width="100%">
+            <img src="${image}" alt="Image" height="50%" width="100%">
                 <div class="details">
                     <div class="mov-name">
                         <span>${finalname}</span>
-                        <span>5.5</span>
+                        <span>${rate}</span>
                     </div>
                     <div class="mov-detail">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, modi?</p>
+                        <p>${aboutm}</p>
                     </div>
                 </div>`;
             add_movie.appendChild(add_div)
